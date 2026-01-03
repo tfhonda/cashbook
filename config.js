799f523b8fcc45a4b838adf58cc657d1e363f945
tfhonda <65443512+tfhonda@users.noreply.github.com>
2026-01-04 00:40:25 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxbKcoETOLtI_eUTJ0Taxty-yiv2n_RC_BFb7d0LK_ikj-FRfCOFbXuj5sYd4FI42he1g/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbz3n8KvJivCF2Exvmrc1hDB5HVEqO29lT26o7SInY6P6Z0N5A1tShyPUYF-uKVpSjL5NA/exec";
